490dfac833361434d126b819716beaf1f0426f7f
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-11-06 18:00:06 +0900

    chore: removing unnecessary files

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,8 @@ import type {UserConfig} from "@commitlint/types";
 
 const Configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  parserPreset: "conventional-changelog-atom",
+  formatter: "@commitlint/format",
   rules: {
     "type-enum": [
       2,
