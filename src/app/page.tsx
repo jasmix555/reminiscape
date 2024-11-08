a7660be75bae886fb2f4522a0254c1a112c74eac
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-11-08 15:07:53 +0900

    chore: create coding environment

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-200">
+      <h1>Reminiscape</h1>
+    </div>
+  );
+}
